dbd3bfad50a6598a095c303e8431291cdeeadfc4
MRizwan1057 <mrizwanr202@gmail.com>
2022-12-27 20:50:45 +0500

    updated

--- Simple_Timer/assets/js/script.js
+++ Simple_Timer/assets/js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     mm = String(today.getMonth() + 1).padStart(2, "0"),
     yyyy = today.getFullYear(),
     nextYear = yyyy + 1,
-    dayMonth = "12/30/",
+    dayMonth = "12/25/",
     birthday = dayMonth + yyyy;
 
   today = mm + "/" + dd + "/" + yyyy;
